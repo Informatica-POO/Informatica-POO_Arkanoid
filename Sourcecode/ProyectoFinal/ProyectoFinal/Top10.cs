21f78eed56163f57c2c6d3b02d9bf3499a01be38
OscarRecinosxd <00128519@uca.edu.sv>
2020-06-10 21:36:46 -0600

    add menu

---
+++ Sourcecode/ProyectoFinal/ProyectoFinal/Top10.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ProyectoFinal
+{
+    public partial class Top10 : Form
+    {
+        public Top10()
+        {
+            InitializeComponent();
+        }
+    }
+}
